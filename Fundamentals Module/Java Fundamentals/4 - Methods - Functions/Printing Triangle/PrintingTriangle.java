bc4dae62fe9da94cffa3b8c8beebfb0289564b65
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-03 07:04:29 +0200

    add

---
+++ Fundamentals Module/Java Fundamentals/4 - Methods - Functions/Printing Triangle/PrintingTriangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrintingTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numLines = Integer.parseInt(scanner.nextLine());
+        printTriangle(numLines);
+    }
+    private static void printLine(int start, int end){
+        for (int i = start; i <= end; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    private static void printTriangle(int n){
+        for (int i = 1; i <= n; i++) {
+            printLine(1, i);
+        }
+        for (int i = n - 1; i >= 1 ; i--) {
+            printLine(1, i);
+        }
+    }
+}
