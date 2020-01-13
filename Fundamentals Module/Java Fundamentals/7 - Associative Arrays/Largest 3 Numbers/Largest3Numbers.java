202ff31c0dc80a2cf5e8f3739b6fb74048644d4c
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-13 06:59:11 +0200

    add

---
+++ Fundamentals Module/Java Fundamentals/7 - Associative Arrays/Largest 3 Numbers/Largest3Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Largest3Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> nums = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).sorted((n1, n2) -> n2.compareTo(n1)).limit(3).collect(Collectors.toList());
+        for (Integer num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
