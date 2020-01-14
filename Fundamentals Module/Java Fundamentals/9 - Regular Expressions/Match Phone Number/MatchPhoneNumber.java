39cc7773bf217e13fdaaa9aa1e9ebc92524cc4cf
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-14 09:25:45 +0200

    add

---
+++ Fundamentals Module/Java Fundamentals/9 - Regular Expressions/Match Phone Number/MatchPhoneNumber.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String regex = "\\+359([- ])2\\1[\\d]{3}\\1[\\d]{4}\\b";
+        String phone = scanner.nextLine();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher phoneMatcher = pattern.matcher(phone);
+        List<String> matchedPhones = new LinkedList<>();
+        while (phoneMatcher.find()) {
+            matchedPhones.add(phoneMatcher.group());
+        }
+        System.out.println(String.join(", ", matchedPhones));
+    }
+}
