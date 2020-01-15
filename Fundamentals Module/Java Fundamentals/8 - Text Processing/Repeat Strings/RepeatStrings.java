1c821a9b7e026c0165e3bf14619ae1ec215ab2f2
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-15 07:40:52 +0200

    add

---
+++ Fundamentals Module/Java Fundamentals/8 - Text Processing/Repeat Strings/RepeatStrings.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RepeatStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] words = scanner.nextLine().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            int count = word.length();
+            for (int i = 0; i < count; i++) {
+                result.append(word);
+            }
+        }
+        System.out.println(result.toString());
+    }
+}
