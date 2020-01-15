2c667e7a7ba189a62c96dfbd3f819dea51479586
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-15 07:39:52 +0200

    add

---
+++ Fundamentals Module/Java Fundamentals/8 - Text Processing/Text Filter/TextFilter.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TextFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] banWords = scanner.nextLine().split(", ");
+        String text = scanner.nextLine();
+        for (String banWord : banWords) {
+            if (text.contains(banWord)) {
+                String replacement = repeatStr("*", banWord.length());
+                text = text.replace(banWord, replacement);
+            }
+        }
+        System.out.println(text);
+    }
+
+    private static String repeatStr(String s, int length) {
+        String replacement = "";
+        for (int i = 0; i < length; i++) {
+            replacement += s;
+        }
+        return replacement;
+    }
+}
