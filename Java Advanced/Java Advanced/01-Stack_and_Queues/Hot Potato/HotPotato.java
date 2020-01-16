3812990b000efaeb6ea0d6d5b18b9f54d7f35391
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-16 18:59:35 +0200

    add

---
+++ Java Advanced/Java Advanced/01-Stack_and_Queues/Hot Potato/HotPotato.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String childName = readChildNames(scanner);
+        String[] splitChildren = childName.split("\\s+");
+        int tossesToRemoveOn = readN(scanner);
+        Deque<String> queue = new ArrayDeque<>(Arrays.asList(splitChildren));
+        int currentRound = 1;
+        while (queue.size() > 1) {
+            String currentChild = queue.poll();
+            if (currentRound % tossesToRemoveOn != 0) {
+                queue.offer(currentChild);
+            } else {
+                System.out.println("Removed " + currentChild);
+            }
+            currentRound++;
+        }
+        System.out.println("Last is " + queue.poll());
+    }
+
+    private static int readN(Scanner scanner) {
+        return scanner.nextInt();
+    }
+
+    private static String readChildNames(Scanner scanner) {
+        return scanner.nextLine();
+    }
+}
