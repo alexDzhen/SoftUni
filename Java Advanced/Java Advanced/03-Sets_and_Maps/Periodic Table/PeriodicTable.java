863777055ac4339201fadf41eb0bc67807d5aaba
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-24 20:29:37 +0200

    add

---
+++ Java Advanced/Java Advanced/03-Sets_and_Maps/Periodic Table/PeriodicTable.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class PeriodicTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        Set<String> compounds = new TreeSet<>();
+        while (n-- > 0) {
+            compounds.addAll(Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toSet()));
+        }
+        compounds.forEach(e -> System.out.print(e + " "));
+    }
+}
