e293e0d4a55fd37bd1099224231df345d6f4511f
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-01-30 07:40:06 +0200

    add

---
+++ Java Advanced/Java Advanced/04-Streams_Files_and_Directories/Get Folder Size/GetFolderSize.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class GetFolderSize {
+    public static void main(String[] args) {
+        String path = "resources2/Exercises Resources";
+        File file = new File(path);
+        long size = 0;
+        for (File innerFile : file.listFiles()) {
+            size += innerFile.length();
+        }
+        System.out.println("Folder size: " + size);
+    }
+}
