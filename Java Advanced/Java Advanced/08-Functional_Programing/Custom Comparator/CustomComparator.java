723a2d7dcb465c8c3596706e0b3ec6eefe4d8540
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-03-26 05:34:41 +0200

    add

---
+++ Java Advanced/Java Advanced/08-Functional_Programing/Custom Comparator/CustomComparator.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class CustomComparator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted(Comparator.comparingInt((Integer f) -> Math.abs(f % 2)).thenComparingInt(f -> f))
+                .map(e -> e + " ")
+                .forEach(System.out::print);
+    }
+}
