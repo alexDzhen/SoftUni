7fdf7d58f776bd7070fa198f1de10c2748b2959a
Aleksandar Dzhenkov <aleksandar.dzhenkov@gmail.com>
2020-03-26 06:08:55 +0200

    add

---
+++ Java Advanced/Java Advanced/Exam Preparation/Dating App/DatingApp.java
@@ -0,0 +1,59 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class DatingApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<Integer> males = new ArrayDeque<>();//stack
+        //MUST USE \PUSH\ FOR STACK
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .forEach(males::push);
+        ArrayDeque<Integer> females = new ArrayDeque<>();//QUE
+        //USE \OFFER\ OR \ADD\ FOR QUE
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .forEach(females::offer);
+        int matches = 0;
+        while (!males.isEmpty() && !females.isEmpty()) {
+            int female = females.peek();
+            int male = males.peek();
+            if (female <= 0) {
+                females.poll();
+                continue;
+            } else if (male <= 0) {
+                males.pop();
+                continue;
+            }
+            if (female % 25 == 0) {
+                females.poll();
+                females.poll();
+                continue;
+            } else if (male % 25 == 0) {
+                males.pop();
+                males.pop();
+                continue;
+            }
+            if (female == male) {
+                matches++;
+                females.poll();
+                males.pop();
+            } else {
+                females.poll();
+                males.push(males.pop() - 2);
+            }
+        }
+        System.out.println("Matches: " + matches);
+        System.out.println("Males left: " + formatArrayDeque(males));
+        System.out.println("Females left: " + formatArrayDeque(females));
+    }
+
+    private static String formatArrayDeque(ArrayDeque<Integer> deque) {
+        if (deque.isEmpty()) {
+            return "none";
+        }
+        return deque.stream().map(String::valueOf).collect(Collectors.joining(", "));
+    }
+}
